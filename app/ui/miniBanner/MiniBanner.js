a1906519bfac660eb1033a653128be35a97a804d
isiagi <isiagigeofrey0@gmail.com>
2024-08-09 11:14:47 +0300

    chore: Add animation

--- app/ui/miniBanner/MiniBanner.js
+++ app/ui/miniBanner/MiniBanner.js
@@ -3,11 +3,14 @@ import React from "react";
 
 function MiniBanner() {
   return (
-    <div className="bg-[url('https://images.unsplash.com/photo-1617864064479-f203fc7897c0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGxheXN0YXRpb258ZW58MHx8MHx8fDA%3D')] h-[200px] md:h-[200px] bg-cover bg-no-repeat bg-center flex items-center">
-      <div className="pl-5">
-        <p>Save Up To 20% Off</p>
-        <h3 className="text-xl mt-3">Santa Lucia Three Seater Sofa</h3>
-        <Button className="bg-[#ff5252] mt-5">Shop Now</Button>
+    <div className="min-h-[200px] overflow-hidden relative group">
+      <div className="group-hover:animate-[zoom_30s_infinite] absolute h-full w-full flex bg-[url('https://images.unsplash.com/photo-1617864064479-f203fc7897c0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cGxheXN0YXRpb258ZW58MHx8MHx8fDA%3D')] bg-cover bg-no-repeat bg-center bg-black bg-blend-overlay bg-opacity-[0.3]"></div>
+      <div className="pl-5 absolute   h-full w-full flex flex-col justify-center">
+        <p className="text-[#ff5252] font-semibold">Save Up To 20% Off</p>
+        <h3 className="text-xl mt-3 text-white">
+          Santa Lucia Three Seater Sofa
+        </h3>
+        <Button className="bg-[#ff5252] mt-5 w-fit">Shop Now</Button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -80,24 +80,22 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        zoom: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "50%": {
+            transform: "scale(1.35)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        zoom: "zoom 10s ease-in-out linear",
       },
     },
   },
